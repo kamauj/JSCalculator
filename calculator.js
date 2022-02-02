8d354263c140bc4dcae4d6d144a9e7db60a2cbad
Jesse <kamauj93@gmail.com>
2022-02-02 16:43:02 +0000

    Added functionality to the buttons

--- calculator.js
+++ calculator.js
@@ -1,2 +1,41 @@
 `use strict`
 import * as DOM from './dom.js';
+
+let  in1 = DOM.inputOne.value;
+let in2 = DOM.inputTwo.value;
+let total = in1 + in2;
+DOM.Add.onclick = () => addition(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.Subtract.onclick = () => subraction(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.Multiply.onclick = () => multiply(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.Divide.onclick = () => divide(DOM.inputOne.value, DOM.inputTwo.value);
+
+function addToList(str) {
+
+    let child = document.createElement(`li`);
+
+    child.classList.add(`list-group-item`);
+
+    child.innerHTML = str;
+
+    DOM.Output.appendChild(child)
+};
+function addition(in1, in2)
+{
+    let result = Number(in1)+Number(in2);
+    addToList(result);
+}
+function subraction(in1, in2)
+{
+    let result = Number(in1)-Number(in2);
+    addToList(result);
+}
+function multiply(in1, in2)
+{
+    let result = Number(in1)*Number(in2);
+    addToList(result);
+}
+function divide(in1, in2)
+{
+    let result = Number(in1)/Number(in2);
+    addToList(result);
+}
--- dom.js
+++ dom.js
@@ -1,1 +1,10 @@
+/* User inputs */
 export const inputOne = document.getElementById(`input_1`);
+export const inputTwo = document.getElementById(`input_2`);
+/* Operations */
+export const Add = document.getElementById(`button_add`);
+export const Subtract = document.getElementById(`button_subtract`);
+export const Multiply = document.getElementById(`button_multiply`);
+export const Divide = document.getElementById(`button_multiply`);
+/* OutPut  */
+export const Output = document.getElementById(`list_output`);
